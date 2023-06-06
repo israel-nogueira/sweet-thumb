8c42e6a1e3c56415a28df5b4b6a79efdcaec75e9
Israel Nogueira <israel@feats.com.br>
2023-06-06 14:46:17 -0300

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'israel-nogueira/sweet-thumb',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1eae560fa3eb3ef33ddc31c9dc56069eef04b51d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'israel-nogueira/sweet-thumb' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1eae560fa3eb3ef33ddc31c9dc56069eef04b51d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
